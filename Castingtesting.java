6d2ecbdd567008dcf02937547ee73899e5d2b437
Jeeban Bashyal <112680722+Gban-jb@users.noreply.github.com>
2025-06-11 21:44:56 -0500

    Add files via upload

---
+++ Castingtesting.java
@@ -0,0 +1,16 @@
+
+public class Castingtesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		boolean b = true;
+		i = int(b);
+		int i = 1;
+		boolean(b) = (boolean) i;
+		
+		
+	}
+
+}
